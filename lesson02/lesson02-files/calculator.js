039205ceb638e4c6aaebcf2af0ae0c89b74ce0e5
mahsieh <michaelhsieh.alt@gmail.com>
2023-06-15 21:32:04 -0700

    added calculator.js with 4 variables and various calculations

---
+++ lesson02/lesson02-files/calculator.js
@@ -0,0 +1,11 @@
+let x = 109;
+let y = 46;
+let z = 27;
+let w = 3.8;
+
+console.log(x * y);
+console.log(x + y);
+console.log(-x * w);
+console.log(z / x);
+console.log(y % z);
+console.log(w ** y);
