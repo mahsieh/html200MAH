d2d3a7da37b2e3d6609a2aadc6df625e3dda9976
mahsieh <michaelhsieh.alt@gmail.com>
2023-06-24 17:49:55 -0700

    created an array with 5 strings, then manipulated it in various ways

---
+++ lesson03/exercise-one.js
@@ -0,0 +1,11 @@
+let arr1 = ['string1', 'string2', 'string3', 'string4', 'string5'];
+console.log(arr1);
+
+arr1.push('string6');
+console.log(arr1);
+
+arr1.splice(2, 1);
+console.log(arr1);
+
+let newString = arr1 + ',';
+console.log(newString);
